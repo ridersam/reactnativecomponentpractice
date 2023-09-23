670709331df0b37c371373fbc5fd2a9160a094a1
Sam Rider <ridersam1023@gmail.com>
2023-09-23 10:25:37 -0500

    Setting up calculator

--- components/Slider.js
+++ components/Slider.js
@@ -1,18 +1,18 @@
 import { Icon, Slider } from 'react-native-elements';
-import { Animated, View, Text } from 'react-native';
+import { Animated, View, Text, StyleSheet } from 'react-native';
 import { useState } from 'react';
 import Constants from 'expo-constants';
 
 const PracticeSlider = (value) => {
     const [state, setState] = useState(5);
-    const [rating, setRating] = useState(5);
-    const [submittedMsg, setSubmittedMsg] = useState('Submitted Rating: ' + rating);
+    const [amount, setAmount] = useState(5);
+    const [submittedMsg, setSubmittedMsg] = useState('Submitted Amount: ' + amount);
 
     return (
         <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center', padding: 10 }}>
             <Slider
                 value={0}
-                onSlidingComplete={(rating) => setRating([rating])}
+                onSlidingComplete={(amount) => setAmount([amount])}
                 minimumValue={20}
                 maximumValue={50}
                 allowTouchTrack
@@ -22,20 +22,26 @@ const PracticeSlider = (value) => {
                 thumbProps={{
                     children: (
                         <Icon
-                            name="heartbeat"
+                            name="dollar"
                             type='font-awesome'
                             size={20}
                             reverse
                             containerStyle={{ bottom:20, right: 20 }}
-                            color='#f50'
+                            color='green'
                         />
                     ),
                 }}
             />
-            <Text>Value: {rating}</Text>
+            <Text>Amount: ${amount}</Text>
         </View>
     );
 }
 
+styles = StyleSheet.create ({
+    amount: {
+        marginTop: 20,
+        fontSize: 24
+    }
+});
 
 export default PracticeSlider;
